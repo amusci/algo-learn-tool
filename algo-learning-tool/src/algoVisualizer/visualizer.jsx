f27081e5a7dea24b5baad27a8ac8cf625a924a82
a <71357098+amusci@users.noreply.github.com>
2024-11-18 18:31:42 -0500

    buttons

--- algo-learning-tool/src/algoVisualizer/visualizer.jsx
+++ algo-learning-tool/src/algoVisualizer/visualizer.jsx
@@ -13,23 +13,48 @@ const SortingVisualizer = () => {
         setArray(newArray);
     };
 
+        
+        const bubbleSort = () => {
+            
+            console.log('Bubble Sort');
+            
+        };
+    
+        const mergeSort = () => {
+            console.log('Merge Sort');
+            
+        };
+    
+        const quickSort = () => {
+            console.log('Quick Sort');
+        };
+
     
     useEffect(() => {
         resetArray();
     }, []); 
 
     return (
-        <div className="array-container">
-            {array.map((value, idx) => (
-                <div
-                    className="array-bar"
-                    key={idx}
-                    style={{ height: `${value}px` }}
-                ></div>
-            ))}
-            <button onClick={resetArray}>Generate New Array</button>
+        <div className="visualizer-container">
+            {/*all the buttons*/}
+            <div className="button-container">
+                <button onClick={resetArray}>Generate New Array</button>
+                <button onClick={bubbleSort}>Bubble Sort</button>
+                <button onClick={mergeSort}>Merge Sort</button>
+                <button onClick={quickSort}>Quick Sort</button>
+            </div>
+
+            {/*bars*/}
+            <div className="array-container">
+                {array.map((value, idx) => (
+                    <div
+                        className="array-bar"
+                        key={idx}
+                        style={{ height: `${value}px` }}
+                    ></div>
+                ))}
+            </div>
         </div>
-        
     );
 };
 
